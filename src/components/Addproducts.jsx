14cc76e219472fdda7e333a782cc62e7c17dd26d
victormuchunu-cyber <victor.muchunu@gmail.com>
2026-03-11 01:35:24 +0300

    first commit

---
+++ src/components/Addproducts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Addproducts = () => {
+  return (
+    <div>
+      <h1>Welcome to Add Products Page.</h1>
+    </div>
+  )
+}
+
+export default Addproducts;
---
+++ src/components/Getproducts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Getproducts = () => {
+  return (
+    <div>
+      <h1>Welcome to the Get Products Page.</h1>
+    </div>
+  )
+}
+
+export default Getproducts;
