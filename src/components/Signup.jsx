5bb178f00bf1c9f61f88f55c997118ce3bc47b30
victormuchunu-cyber <victor.muchunu@gmail.com>
2026-03-19 12:32:22 +0300

    first commit

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,3 +1,4 @@
+import axios from 'axios';
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom';
 
@@ -7,12 +8,71 @@ const Signup = () => {
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
   const [phone, setPhone] = useState("")
+
+  // Define the three states an application will move to
+  const [loading, setLoading] = useState("")
+  const [success, setSuccess] = useState("")
+  const [error, setError] = useState("")
+  
+  // Below is a function that will handle the submit action.
+  const handleSubmit =async(e) =>{
+    // Below we prevent our site from reloading.
+    e.preventDefault()
+
+    // Update our loading hook with a message that will be displayed to the users who are trying to register.
+    setLoading("Please wait as registration is in progress...")
+
+    try{
+      // Create a form that will enable you to capture the four details entered on the form.
+      const formdata = new FormData();
+      
+      // insert the four details (username, email, password, phone) interms of key - value pairs
+      formdata.append("username", username);
+      formdata.append("email", email);
+      formdata.append("password", password);
+      formdata.append("phone", phone);
+
+      // By use of axios, we can access the method post
+      const response =await axios.post("https://victor.alwaysdata.net/api/signup", formdata)
+
+      // Set back the loading to default
+      setLoading("");
+
+      // Just incase everything goes on well, update the success hook with a message.
+      setSuccess(response.data.message)
+
+      // clear your hooks
+      setUsername("");
+      setEmail("");
+      setPassword("");
+      setPhone("");
+    }
+    catch(error){
+      // Set the loading back to default
+      setLoading("");
+
+      // Update the error hook with the message given back from the response.
+      setError(error.message)
+
+    }
+  }
+
+
+
+
+
+
   return (
     <div className='row justify-content-center mt-4 '>
       <div className="card col-md-6 shadow p-4 ">
         <h1 className='text-primary'>Sign Up</h1>
+        <h5 className="text-warning"> {loading} </h5>
+        <h3 className="text-success">{success}</h3>
+        <h4 className="text-danger">{error}</h4>
 
-        <form >
+
+
+        <form onSubmit={handleSubmit}>
           <input type="text"
            placeholder='Enter the Username'
            className='form-control'
@@ -50,7 +110,7 @@ const Signup = () => {
 
            {/* {phone} */}
 
-           <input type="button" value="Signup" className='btn btn-primary'/>
+           <input type="submit" value="Signup" className='btn btn-primary'/>
            <br /><br />
 
            Already have an account? <Link to={'/signin'}>Signin</Link>
@@ -63,3 +123,9 @@ const Signup = () => {
 export default Signup;
 
 // Research on Axios in reactjs
+// Axios is a promise-based HTTP client used in JavaScript and React applications to communicate with APIs.
+// It allows your React app to send requests to servers and receive data such as JSON.
+// In simple terms:
+// Axios helps React apps fetch data, send data, update data, or delete data from a backend server or API. 
+// Axios is preferred because it is easier and more powerful than the native JavaScript Fetch API.
+// TO install = npm install axios
